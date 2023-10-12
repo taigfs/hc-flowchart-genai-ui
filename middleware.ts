050813299f2dd16ad4b588f8d4e133177d59d3a7
Tainan Fidelis <taigfs@gmail.com>
2023-10-12 15:09:00 -0300

    Refactor protected page to use next-auth/next for server session and add navbar component.

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,10 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/protected") && token === null) {
+      // if (req.nextUrl.pathname.startsWith("/protected") && token === null) {
+      //   return false;
+      // }
+      if (token === null) {
         return false;
       }
       return true;
