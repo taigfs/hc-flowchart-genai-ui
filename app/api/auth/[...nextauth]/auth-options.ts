f7fee8fb419d3f8fe8f780d233d7a82b96956335
Tainan Fidelis <taigfs@gmail.com>
2023-10-12 14:44:40 -0300

    Added auth options;

---
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+}
