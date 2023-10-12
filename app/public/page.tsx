f6f1a276f94c2d4d5b2a4c3ac7c91ce073febb61
Tainan Fidelis <taigfs@gmail.com>
2023-10-12 13:35:09 -0300

    Adding navbar;

--- app/public/page.tsx
+++ app/public/page.tsx
@@ -1,21 +1,25 @@
 import { getServerSession } from "next-auth/next"
 import type { NextRequest } from "next/server"
 import { authOptions } from "../api/auth/[...nextauth]/route"
+import { Navbar } from "@/components/navbar"
 
 export default async function Protected (req: NextRequest): Promise<any> {
   const session = await getServerSession(authOptions)
-
+console.log(session);
   return (
-    <div className='grid grid-cols-2 text-white p-4'>
-      <div>
-        {
-          session !== null
-            ? <h1 className='leading-loose text-[15rem] font-extrabold text-accent'>
-                Hi {session?.user?.name}!
-              </h1>
-            : <a className='btn btn-primary' href='/api/auth/signin'>Sign in</a>
-        }
+    <>
+      <Navbar />
+      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+        <div>
+          {
+            session !== null
+              ? <span className='leading-loose text-[2rem] font-extrabold text-accent'>
+                  Hi {session?.user?.name}!
+                </span>
+              : <a className='btn btn-primary' href='/api/auth/signin'>Sign in</a>
+          }
+        </div>
       </div>
-    </div>
+    </>
   )
 }
