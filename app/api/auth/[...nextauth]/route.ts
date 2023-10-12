6d4a681439173bf7b190f1316b54f07fa4f59a80
Tainan Fidelis <taigfs@gmail.com>
2023-10-12 14:31:52 -0300

    Refactor authentication options to use environment variables for GitHub credentials.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
-  // Configure one or more authentication providers
+const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
     }),
-    // ...add more providers here
   ],
 }
 
