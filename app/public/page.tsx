6d0ef4a05e595fc8996ee211d81b6048296c87d8
Tainan Fidelis <taigfs@gmail.com>
2023-10-12 14:38:04 -0300

    Remove unused import and parameter from Protected function.

--- app/public/page.tsx
+++ app/public/page.tsx
@@ -1,9 +1,9 @@
 import { getServerSession } from "next-auth/next"
-import type { NextRequest } from "next/server"
+// import type { NextRequest } from "next/server"
 import { Navbar } from "@/components/navbar"
 import { authOptions } from "../api/auth/[...nextauth]/auth-options";
 
-export default async function Protected (req: NextRequest): Promise<any> {
+export default async function Protected (): Promise<any> {
   const session = await getServerSession(authOptions)
 console.log(session);
   return (
