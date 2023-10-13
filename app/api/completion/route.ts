27586c53a304c116f26cd9f2256a90cc6a6c354c
Tainan Fidelis <taigfs@gmail.com>
2023-10-13 14:54:23 -0300

    (no commit message provided)

---
+++ app/api/completion/route.ts
@@ -0,0 +1,35 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response: any = await openai.completions.create({
+    model: 'text-davinci-003',
+    stream: true,
+    temperature: 0.6,
+    max_tokens: 300,
+    prompt: `Create three slogans for a business with unique features.
+ 
+Business: Bookstore with cats
+Slogans: "Purr-fect Pages", "Books and Whiskers", "Novels and Nuzzles"
+Business: Gym with rock climbing
+Slogans: "Peak Performance", "Reach New Heights", "Climb Your Way Fit"
+Business: ${prompt}
+Slogans:`,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import { Navbar } from '@/components/ui/navbar';
 import Diagram from '@/components/reactflow/diagram';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+import { useCompletion } from 'ai/react';
 
-export default async function Protected(): Promise<any> {
+export default function Protected() {
+  const { completion, input, handleInputChange, handleSubmit } =
+    useCompletion();
   return (
     <>
       <Navbar />
-      <div
+      <form
+        onSubmit={handleSubmit}
         className='mx-auto max-w-7xl px-1 sm:px-6 lg:px-8 flex py-3'
         style={{ height: 'calc(100% - 110px)' }}
       >
@@ -17,15 +23,20 @@ export default async function Protected(): Promise<any> {
               <Textarea
                 placeholder='Type here...'
                 className='flex-grow rounded-b-none focus:outline-none'
+                value={input}
+                onChange={handleInputChange}
               />
-              <Button className='rounded-t-none'>Submit</Button>
+              <div>{completion}</div>
+              <Button className='rounded-t-none' type='submit'>
+                Submit
+              </Button>
             </div>
           </div>
           <div className='bg-gray-900 p-4 rounded'>
             <Diagram />
           </div>
         </div>
-      </div>
+      </form>
       <div className='text-center text-gray-500 text-sm pt-2'>
         Created by{' '}
         <a href='https://hookcaptain.com' className='underline'>
