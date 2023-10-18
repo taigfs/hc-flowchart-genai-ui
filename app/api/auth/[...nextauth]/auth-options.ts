ab30e37c0b94e266943155c0a0bdb66fb607be79
Tainan Fidelis <taigfs@gmail.com>
2023-10-18 16:42:08 -0300

    Add logo to the authentication options.

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -1,11 +1,14 @@
-import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
-}
+  theme: {
+    logo: 'https://hookcaptain.s3.sa-east-1.amazonaws.com/hc-logo.png',
+  },
+};
